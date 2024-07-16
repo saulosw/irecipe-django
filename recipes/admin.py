2c040f5f6a24e2a1df8c6fc6e4f945ff7edecbe1
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-07-16 19:31:22 -0300

    admin-configs

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+class RecipeAdimin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdimin)
--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self) -> str:
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
@@ -21,3 +24,6 @@ class Recipe(models.Model):
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.title
