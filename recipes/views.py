56bb2e12e708f7807955f9397254593161264b41
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-07-17 17:21:06 -0300

    category-changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 
 from utils.recipes.factory import make_recipe
 from .models import Recipe
@@ -9,14 +9,19 @@ def home(request):
         'recipes': recipes,
     })
 
+
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))
+
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 def recipes(request, id):
+    recipe = Recipe.objects.filter(pk=id, is_published=True).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
