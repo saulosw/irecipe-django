050ebaf72efff4a5bf23fa67af4cb0ea815748f1
Saulo Pereira Jesus <saulopereiradejesus@gmail.com>
2024-07-18 17:59:23 -0300

    tests-urls

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    def test_url_home_is_working(self):
+        home_url = reverse('recipes:home')
+        self.assertEqual(home_url, '/')
+    
+    def test_url_category_is_working(self):
+        category_url = reverse('recipes:category', kwargs={'category_id':1})
+        self.assertEqual(category_url, '/recipes/category/1/')
+
+    def test_url_recipe_is_working(self):
+        recipe_url = reverse('recipes:recipe', kwargs={'id':1})
+        self.assertEqual(recipe_url, '/recipes/1/')
